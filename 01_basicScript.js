66063004346fa8271c2f6953b2523441419f3ac2
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-07-26 14:20:34 +0200

    script executed

--- 01_basicScript.js
+++ 01_basicScript.js
@@ -1,10 +1,11 @@
-const {chromium} = require('playwright');
+const { chromium } = require("playwright");
 
 (async () => {
-    const browser =await chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://todomvc.com/examples/react/#/');
-    await page.screenshot({path: `todo.png`});
-    await browser.close();
-})
+  const browser = await chromium.launch({ headless: false });
+  // const context = await browser.newContext()
+  // const page = await context.newPage()
+  const page = await browser.newPage();
+  await page.goto("http://todomvc.com/examples/react/#/");
+  await page.screenshot({ path: `todo.png` });
+  await browser.close();
+})();
